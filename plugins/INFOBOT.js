f7e4ccf7fe6c559a35c774f5f7e12948d5ee06a1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-05 21:56:45 -0500

    Update INFOBOT.js

--- plugins/INFOBOT.js
+++ plugins/INFOBOT.js
@@ -1,4 +1,6 @@
-
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let fs = require('fs')
 let handler = async (m, { conn }) => {
     let gt = global.gt
     let _uptime = process.uptime() * 1000
@@ -20,7 +22,7 @@ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
     `const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
-           hydratedContentText: esce,
+           hydratedContentText: str,
            locationMessage: { 
            jpegThumbnail: await (await fetch(fla + bear)).buffer() },           
            hydratedFooterText: wm,
@@ -39,6 +41,15 @@ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
 
            }]
          }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
 }
 handler.help = ['botstatus']
 handler.tags = ['info']

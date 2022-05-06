29609c38f9e09448449ac83a7253b5b6ef143e85
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-05 19:29:51 -0500

    Update database.js

--- plugins/database.js
+++ plugins/database.js
@@ -1,8 +1,4 @@
-let handler = async (m) => {
-    //let rl = global.rl
-    //global.rl = '〘 ✅ 〙 ╰⊱✨⊱ *𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊 | 𝙍𝙀𝙎𝙐𝙇𝙏* ⊱✨⊱╮'
-    //global.resul = rl
-   
+let handler = async (m) => { 
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
    m.reply(conn.sendBut(m.chat, `${rl} *En la base de datos hay ${totalreg} Usuarios*.\n*In the database I have registered ${totalreg} Users*.`, gt , `☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩`, '.menu',  m))

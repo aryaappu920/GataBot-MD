eb4a84c8df7bcd8ada26388a3865d148e78744ff
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-08 00:47:38 -0500

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,4 +1,29 @@
 let { execSync } = require('child_process')
+let handler = async (m, { conn, text, isROwner }) => {
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git remote set-url origin https://github.com/Paquito1923/elyasmd.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
+  }
+}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = /^update$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
+
+/*let { execSync } = require('child_process')
 let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
@@ -22,3 +47,4 @@ handler.fail = null
 handler.exp = 0
 
 module.exports = handler
+*/

78f5f76fca9a6c7c0c585696eb6fd2e70dbeb72a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-08 13:13:33 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,5 +1,5 @@
-let handler = async(m, { conn, text, usedPrefix: _p, args, command }) => {
-    if (!text) throw '╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n*Escriba un reporte.*\n*Ejemplo:*\n${usedPrefix + command} *El comando ${_p}?serbot no funciona.*\n\n*Write a report.*\n*Example*\n${usedPrefix + command} *The command${usedPrefix}jadibot it does not work*'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n*Escriba un reporte.*\n*Ejemplo:*\n${usedPrefix + command} *El comando ${usedPrefix}serbot no funciona.*\n\n*Write a report.*\n*Example*\n${usedPrefix + command} *The command${usedPrefix}jadibot it does not work.*`
     if (text.length > 1500) throw 'Maaf Teks Terlalu Panjang, Maksimal 1500 Teks!'
     const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6281515860089@s.whatsapp.net'))

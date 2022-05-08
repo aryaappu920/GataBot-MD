d0812edb94da5a25d75cfafd859496afda5bcc58
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-08 13:51:59 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,7 +1,8 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
+    wm = global.wm
     if (!text) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n*Escriba un reporte.*\n*Ejemplo:*\n${usedPrefix + command} *El comando ${usedPrefix}serbot no funciona.*\n\n*Write a report.*\n*Example*\n${usedPrefix + command} *The command${usedPrefix}jadibot it does not work.*`
     if (text.length > 1500) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*El reporte minimo es de 1500 caracteres!*\n*The minimum report must be 1500 characters!*'
-    const laporan = `╭━━━━〔 *REPORTE | REPORT* 〕━━━⬣*\n┃\n┃ *Número | Number*\n┃ *Wa.me/${m.sender.split`@`[0]}*\n┃\n┃ *Mensaje | Text*\n┃ ${text}\n┃\n┃╰━━━━〔 *${wm}* 〕━━━━━⬣`
+    const laporan = `╭━━━━〔 *REPORTE | REPORT* 〕━━━⬣*\n┃\n┃ *Número | Number*\n┃ *Wa.me/${m.sender.split`@`[0]}*\n┃\n┃ *Mensaje | Text*\n┃ ${text}\n┃\n╰━━━━〔 *${wm}* 〕━━━━━⬣`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '593968585383@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe

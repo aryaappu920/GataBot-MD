3535abd5991044c9768a8fc8023f323c8b7100e2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-08 13:20:39 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,6 +1,6 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n*Escriba un reporte.*\n*Ejemplo:*\n${usedPrefix + command} *El comando ${usedPrefix}serbot no funciona.*\n\n*Write a report.*\n*Example*\n${usedPrefix + command} *The command${usedPrefix}jadibot it does not work.*`
-    if (text.length > 1500) throw 'Maaf Teks Terlalu Panjang, Maksimal 1500 Teks!'
+    if (text.length > 1500) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*El reporte minimo tiene que ser de 1500 caracteres!*\n*The minimum report must be 1500 characters!*'
     const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6281515860089@s.whatsapp.net'))
     m.reply(laporan, jid)

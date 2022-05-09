df7ebb21fe91098f30b0878fb8c06f26aa1cad54
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-08 22:53:57 -0500

    Update qrcode.js

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -1,27 +1,5 @@
 let qrcode = require("qrcode")
 
-let handler  = async (m, { conn, text }) => {
-  conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
-}
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
-handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
-module.exports = handler
-
-
-/*let qrcode = require("qrcode")
-
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'teksnya mana?'
   conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '', m)
@@ -35,4 +13,4 @@ handler.botAdmin = false
 handler.fail = null
 
 module.exports = handler
-*/
+
